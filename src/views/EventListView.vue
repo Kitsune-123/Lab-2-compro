47fb5acc788756b9dea122905ed7fddc7d4fe519
tatchapol <tatchapon58@gmail.com>
2024-08-09 17:43:44 +0700

    Fix(tailwind install new npm Lab 5 @2.3)

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -45,7 +45,7 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <!-- new element-->
    
- <div class="events">
+ <div class="flex flex-col items-center">
   <EventCard v-for="event in events" :key="event.id" :event="event"/>
 
   <div class=" pagination">
@@ -69,11 +69,7 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+
 .pagination {
   display: flex;
   width: 290px;
