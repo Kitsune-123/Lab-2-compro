aab1947a39e7d1e512806c35dc0f0a6d1d0c3cc5
tatchapol <tatchapon58@gmail.com>
2024-08-16 09:48:00 +0700

    Update server

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,18 +1,24 @@
 import axios from 'axios'
-
+ 
 const apiClient = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/saran1185a/databaseForLab2',
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://my-json-server.typicode.com/SammyEiei/my_Server_lab2',
+  //   baseURL: 'https://dv-student-backend-2019.appspot.com/',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
+ 
 export default {
-    getEvents(perPage: Number, page: Number) {
-        return apiClient.get('/events?_limit='+ perPage + '&_page='+ page)
-    },
-    getEvent (id: number){
-        return apiClient.get('/events/' + id)
-    }
+  // getEvents() {
+  //   return apiClient.get('/events')
+  getEvents(perPage: number, page: number) {
+    return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
+  },
+ 
+  getEvent(id: number) {
+    return apiClient.get(`/events/${id}`)
+  }
 }
+ 
